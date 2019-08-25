c065489a3dfff4957464b4e7db5062c53d2db746
ridergit <benjaminriderharvey@gmail.com>
2019-08-25 10:33:26 -0400

    Completes step 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import {routes, airlines, airports} from './data.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <header className="header">
-          <h1 className="title">Airline Routes</h1>
-        </header>
-        <section>
-          <p>
-            Welcome to the app!
-          </p>
-        </section>
+      <div>
+        <table class="routes-table">
+            <thead>
+              <tr>
+                <th>Airline</th>
+                <th>Source Airport</th>
+                <th>Destination Airport</th>
+              </tr>
+            </thead>
+            <tbody>
+              {routes.map(route => (
+              <tr >
+                <td>{route.airline}</td>
+                <td>{route.src}</td>
+                <td>{route.dest}</td>
+              </tr>
+             ))}
+          </tbody>
+        </table>
       </div>
     );
   }
